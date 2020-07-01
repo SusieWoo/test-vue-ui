cc57d28151f5adcb272e059f59dd72680b7deea1
wurui <wurui@aerozhonghuan.com>
2020-07-01 13:50:23 +0800

    'name'

--- src/views/app/business/app-config/screen/index.vue
+++ src/views/app/business/app-config/screen/index.vue
@@ -1,25 +1,58 @@
 <template>
-  <div class="app-container">
-    2342342342342
-  <div style="width:100px;height:100px;border:1px solid red;">
-  </div>
-  </div>
+  <el-container class="view-form">
+    <el-form ref="form"
+             :rules="rules"
+             label-width="220px"
+             :model="row">
+      <el-row type="flex"
+              justify="center">
+        <el-col :span="24">
+          <el-card shadow="hover">
+            <div slot="header">
+              <span>信息</span>
+            </div>
+            <el-col :span="18"
+                    :offset="2">
+              <el-form-item label="上传">
+                <el-upload multiple
+                           :limit="10">
+                  <el-button size="small"
+                             type="primary">
+                    点击上传
+                  </el-button>
+                  <div slot="tip"
+                       class="el-upload__tip">
+                    提示：最大支持5M文件，如果文件较大，请先压缩文件
+                  </div>
+                </el-upload>
+              </el-form-item>
+            </el-col>
+          </el-card>
+        </el-col>
+      </el-row>
+    </el-form>
+  </el-container>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-
-@Component({
-  name: 'screen'
-})
-export default class extends Vue {
-
-
-
-
-
-
-
-
+<script>
+export default {
+  data () {
+    return {
+      finishUpload: true,
+      fullscreenLoading: false,
+      time: '',
+      endDate: '0',
+      fileObjec: [],
+      fileList: [],
+      options: [
+        { label: '1', value: 1 },
+        { label: '自定义', value: 0 }
+      ]
+    }
+  },
+  mounted () {
+  },
+  methods: {
+  }
 }
 </script>
