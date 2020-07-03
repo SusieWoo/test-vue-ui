782823655db5b1ceb84a598146b52cb2d97d31c3
lianxue <lianxueko@qq.com>
2020-07-03 10:42:32 +0800

    commit

---
+++ src/components/comSearch.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="com-search">
+    <el-form-item >
+      <el-button type="text" :icon="[isShow?'el-icon-caret-bottom':'el-icon-caret-top']" @click="toggleShow" >高级筛选</el-button>
+    </el-form-item>
+    <el-card v-if="isShow" class="card-panel">
+      <slot name="item"></slot>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "comSearch",
+  props: {
+    // 接收绑定参数
+    value: [String, Array]
+  },
+  data() {
+    return {
+        isShow:false
+    };
+  },
+  watch: {},
+  methods: {
+      toggleShow(){
+          this.isShow = !this.isShow
+      }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.com-search{
+  display: inline;
+  margin-left: 10px;
+}
+</style>
--- src/views/businessSupport/vehicleManage.vue
+++ src/views/businessSupport/vehicleManage.vue
@@ -6,7 +6,7 @@
       <el-tab-pane label="解绑记录" name="0"></el-tab-pane>
     </el-tabs>
     <el-card class="card-panel">
-      <el-form :inline="true" :model="querys">
+      <el-form :inline="true" :model="querys" label-position="left">
         <el-form-item label="车辆VIN">
           <el-input v-model="querys.vin" placeholder="请输入车辆VIN" />
         </el-form-item>
@@ -21,78 +21,90 @@
           <el-button type="primary" icon="el-icon-search" @click="getList">搜索</el-button>
         </el-form-item>
 
-        <el-collapse v-model="activeName" accordion class="collapse-select">
-          <el-collapse-item title="高级筛选" name="1">
-            <el-form-item label="车辆首次绑定">
-              <el-select v-model="querys.firstBind" class="search-item" placeholder="请选择">
-                <el-option
-                  v-for="item in bindList"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value"
-                />
-              </el-select>
-            </el-form-item>
-            <el-form-item label="处理时间">
-              <el-date-picker v-model="querys.excuteStart"  class="search-item" type="date" placeholder="选择起始日期"></el-date-picker>
-            </el-form-item>
-            <el-form-item label="至">
-              <el-date-picker v-model="querys.excuteEnd"  class="search-item" type="date" placeholder="选择截止日期"></el-date-picker>
-            </el-form-item>
-            <el-form-item label="车系">
-              <multiple-select :width="160" isLabelData :data="treeData" v-model="querys.series"></multiple-select>
-            </el-form-item>
-            <el-form-item label="车型">
-              <el-select v-model="querys.type" multiple class="search-item" placeholder="请选择车系">
-                <el-option
-                  v-for="(item,index) in seriesList "
-                  :key="index"
-                  :label="item"
-                  :value="index"
-                />
-              </el-select>
-            </el-form-item>
-            <el-form-item label="排放：">
-              <el-input v-model="querys.dischargeId" placeholder="请选择排放" />
-            </el-form-item>
+        <com-search>
+          <template>
+            <span style="" slot="item">
+              <el-form-item label="车辆首次绑定">
+                <el-select v-model="querys.firstBind" class="search-item" placeholder="请选择">
+                  <el-option
+                    v-for="item in bindList"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  />
+                </el-select>
+              </el-form-item>
+              <el-form-item label="处理时间">
+                <el-date-picker
+                  v-model="querys.excuteStart"
+                  class="search-item"
+                  type="date"
+                  placeholder="选择起始日期"
+                ></el-date-picker>
+              </el-form-item>
+              <el-form-item label="至">
+                <el-date-picker
+                  v-model="querys.excuteEnd"
+                  class="search-item"
+                  type="date"
+                  placeholder="选择截止日期"
+                ></el-date-picker>
+              </el-form-item>
+              <el-form-item label="车系">
+                <multiple-select :width="160" isLabelData :data="treeData" v-model="querys.series"></multiple-select>
+              </el-form-item>
+              <el-form-item label="车型">
+                <el-select v-model="querys.type" multiple class="search-item" placeholder="请选择车系">
+                  <el-option
+                    v-for="(item,index) in seriesList "
+                    :key="index"
+                    :label="item"
+                    :value="index"
+                  />
+                </el-select>
+              </el-form-item>
+              <el-form-item label="排放：">
+                <el-input v-model="querys.dischargeId" placeholder="请选择排放" />
+              </el-form-item>
 
-            <el-form-item label="当前车辆状态">
-              <el-select v-model="querys.arStatus" class="search-item" placeholder="请选择当前车辆状态">
-                <el-option
-                  v-for="item in bindList"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value"
-                />
-              </el-select>
-            </el-form-item>
-            <el-form-item label="所属省：">
-              <el-select
-                v-model="querys.provinceId"
-                @change="changeArea"
-                class="search-item"
-                placeholder="请选择省"
-              >
-                <el-option
-                  v-for="item in ProvinceList "
-                  :key="item.id"
-                  :label="item.name"
-                  :value="item.id"
-                />
-              </el-select>
-            </el-form-item>
-            <el-form-item label="市：">
-              <el-select v-model="querys.cityId" class="search-item" placeholder="请选择市">
-                <el-option
-                  v-for="item in cityList "
-                  :key="item.id"
-                  :label="item.name"
-                  :value="item.id"
-                />
-              </el-select>
-            </el-form-item>
-          </el-collapse-item>
-        </el-collapse>
+              <el-form-item label="当前车辆状态">
+                <el-select v-model="querys.arStatus" class="search-item" placeholder="请选择当前车辆状态">
+                  <el-option
+                    v-for="item in bindList"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  />
+                </el-select>
+              </el-form-item>
+              <el-form-item label="所属省：">
+                <el-select
+                  v-model="querys.provinceId"
+                  @change="changeArea"
+                  class="search-item"
+                  placeholder="请选择省"
+                >
+                  <el-option
+                    v-for="item in ProvinceList "
+                    :key="item.id"
+                    :label="item.name"
+                    :value="item.id"
+                  />
+                </el-select>
+              </el-form-item>
+              <el-form-item label="市：">
+                <el-select v-model="querys.cityId" class="search-item" placeholder="请选择市">
+                  <el-option
+                    v-for="item in cityList "
+                    :key="item.id"
+                    :label="item.name"
+                    :value="item.id"
+                  />
+                </el-select>
+              </el-form-item>
+            </span>
+          </template>
+        </com-search>
       </el-form>
     </el-card>
   </div>
@@ -105,9 +117,11 @@ import {
 } from "@/api/business/businessService";
 
 import multipleSelect from "@/components/multipleSelect";
+import comSearch from "@/components/comSearch";
 export default {
   components: {
-    multipleSelect
+    multipleSelect,
+    comSearch
   },
   data() {
     return {
@@ -172,8 +186,5 @@ export default {
 <style lang="scss" scoped>
 .collapse-select {
   padding: 0 20px;
-}
-.search-item {
-  width: 160px;
 }
 </style>
