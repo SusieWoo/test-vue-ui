86a1743802cc0dd1461d27cd1c8b5dd83b62d6d3
wurui <wurui@aerozhonghuan.com>
2020-07-06 17:30:50 +0800

    用 dev

--- src/api/business/businessService.ts
+++ src/api/business/businessService.ts
@@ -3,64 +3,60 @@ import Mock from './mock'
 const isMock = true;
 export const queryCarPropertyForMaintains = () =>
   request({
-    url: 'http://sy.aerozhonghuan.com:81/test/qdfaw/api/qingqi/sellbusiness/queryCarPropertyForMaintains',
+    url: 'http://sy.aerozhonghuan.com:81/dev/qdfaw/api/qingqi/sellbusiness/queryCarPropertyForMaintains',
     method: 'post',
     data: {
-      propertyName: "1"
+      propertyName: '1'
     }
   });
 export const queryArea = (id: any) => {
   return request({
-    url: 'http://sy.aerozhonghuan.com:81/test/qdfaw/api/qingqi/servicestation/activity/queryArea',
+    url: 'http://sy.aerozhonghuan.com:81/dev/qdfaw/api/qingqi/servicestation/activity/queryArea',
     method: 'post',
     data: {
       provinceId: id
     }
   });
-
 }
 export const carBindList = (data: any) => {
-  if (isMock){
+  if (isMock) {
     return Mock.carBindList();
   }
-  return  request({
+  return request({
     url: 'http://sy.aerozhonghuan.com:81/test/yiqi/app/api/faw/operate/review/carBindList',
     method: 'post',
     data: data
   });
-
 }
 export const carBindDetail = (data: any) => {
   // if (isMock){
   //   return Mock.carBindList();
   // }
-  return  request({
+  return request({
     url: 'http://sy.aerozhonghuan.com:81/test/yiqi/app/api/faw/operate/review/carBindDetail',
     method: 'post',
     data: data
   });
-
 }
 export const carUnBind = (data: any) => {
   // if (isMock){
   //   return Mock.carBindList();
   // }
-  return  request({
+  return request({
     url: 'http://sy.aerozhonghuan.com:81/test/yiqi/app/api/faw/operate/review/carUnBind',
     method: 'post',
     data: data
   });
 }
 export const carTeamList = (data: any) => {
-  if (isMock){
+  if (isMock) {
     return Mock.carTeamList();
   }
-  return  request({
+  return request({
     url: 'http://sy.aerozhonghuan.com:81/test/yiqi/app/api/faw/operate/carTeam/carTeamList',
     method: 'post',
     data: data
   });
-
 }
 
 
