b2a0a464ed723a78734492b2e30725491d4d15f8
sunzengrong <sunzengrong@aerozhonghuan.com>
2020-07-08 19:17:15 +0800

    消息管理 - 推送消息

---
+++ src/api/business/messageService.ts
@@ -0,0 +1,48 @@
+import request from '@/utils/request';
+
+
+export const noticeList = (data: any) => {
+    return request({
+        url: 'http://sy.aerozhonghuan.com:81/test/yiqi/app/api/faw/operate/notice/queryNoticeList',
+        method: 'post',
+        data: data
+    });
+};
+
+export const brandList = (params: any) =>
+    request({
+        url: 'http://sy.aerozhonghuan.com:81/test/yiqi/app/api/faw/operate/common/brandList',
+        method: 'get',
+        params
+    });
+
+export const carSeriesList = (params: any) =>
+    request({
+        url: 'http://sy.aerozhonghuan.com:81/test/yiqi/app/api/faw/operate/common/seriseList',
+        method: 'get',
+        params
+    });
+
+export const carTypeList = (params: any) =>
+    request({
+        url: 'http://sy.aerozhonghuan.com:81/test/yiqi/app/api/faw/operate/common/activityModelList',
+        method: 'get',
+        params
+    });
+
+export const newNotice = (data: any) => {
+    return request({
+        url: 'http://sy.aerozhonghuan.com:81/test/yiqi/app/api/faw/operate/notice/newNotice',
+        method: 'post',
+        data: data
+    });
+};
+
+
+export const resendNotice = (data: any) => {
+    return request({
+        url: 'http://sy.aerozhonghuan.com:81/test/yiqi/app/api/faw/operate/notice/resendNotice',
+        method: 'post',
+        data: data
+    });
+};
