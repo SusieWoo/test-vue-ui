de2e8d891ebf085087fd2866ee99920afc784d22
wurui <wurui@aerozhonghuan.com>
2020-07-08 20:27:49 +0800

    目录

--- src/router/modules/nested.ts
+++ src/router/modules/nested.ts
@@ -19,26 +19,26 @@ const nestedRoutes: RouteConfig = {
       children: [
         {
           path: 'menu1-1',
-          component: () => import(/* webpackChunkName: "menu1-1" */ '@/views/nested/menu1/menu1-1/index.vue'),
+          component: () => import(/* webpackChunkName: "menu1-1" */ '@/views/example/nested/menu1/menu1-1/index.vue'),
           name: 'Menu1-1',
           meta: { title: 'menu1-1' }
         },
         {
           path: 'menu1-2',
-          component: () => import(/* webpackChunkName: "menu1-2" */ '@/views/nested/menu1/menu1-2/index.vue'),
+          component: () => import(/* webpackChunkName: "menu1-2" */ '@/views/example/nested/menu1/menu1-2/index.vue'),
           redirect: '/nested/menu1/menu1-2/menu1-2-1',
           name: 'Menu1-2',
           meta: { title: 'menu1-2' },
           children: [
             {
               path: 'menu1-2-1',
-              component: () => import(/* webpackChunkName: "menu1-2-1" */ '@/views/nested/menu1/menu1-2/menu1-2-1/index.vue'),
+              component: () => import(/* webpackChunkName: "menu1-2-1" */ '@/views/example/nested/menu1/menu1-2/menu1-2-1/index.vue'),
               name: 'Menu1-2-1',
               meta: { title: 'menu1-2-1' }
             },
             {
               path: 'menu1-2-2',
-              component: () => import(/* webpackChunkName: "menu1-2-2" */ '@/views/nested/menu1/menu1-2/menu1-2-2/index.vue'),
+              component: () => import(/* webpackChunkName: "menu1-2-2" */ '@/views/example/nested/menu1/menu1-2/menu1-2-2/index.vue'),
               name: 'Menu1-2-2',
               meta: { title: 'menu1-2-2' }
             }
@@ -46,7 +46,7 @@ const nestedRoutes: RouteConfig = {
         },
         {
           path: 'menu1-3',
-          component: () => import(/* webpackChunkName: "menu1-3" */ '@/views/nested/menu1/menu1-3/index.vue'),
+          component: () => import(/* webpackChunkName: "menu1-3" */ '@/views/example/nested/menu1/menu1-3/index.vue'),
           name: 'Menu1-3',
           meta: { title: 'menu1-3' }
         }
@@ -54,7 +54,7 @@ const nestedRoutes: RouteConfig = {
     },
     {
       path: 'menu2',
-      component: () => import(/* webpackChunkName: "menu2" */ '@/views/nested/menu2/index.vue'),
+      component: () => import(/* webpackChunkName: "menu2" */ '@/views/example/nested/menu2/index.vue'),
       name: 'Menu2',
       meta: { title: 'menu2' }
     }
