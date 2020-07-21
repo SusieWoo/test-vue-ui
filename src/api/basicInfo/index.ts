8de986d9c4571a52d26951e7aaa9e421a8cd8a2b
lianxue <lianxueko@qq.com>
2020-07-21 10:33:47 +0800

    no message

--- src/api/basicInfo/index.ts
+++ src/api/basicInfo/index.ts
@@ -1,52 +1,51 @@
 import request, { post } from '@/utils/request';
-const base = process.env.VUE_APP_TSP_API
 
 export const queryCar = (params: any) =>
   request({
-    url:'system/car/queryCar',
+    url: 'system/car/queryCar',
     method: 'get',
     params
   });
 
 export const queryCarBasicData = (params: any) =>
   request({
-    url:'system/car/queryBasicData',
+    url: 'system/car/queryBasicData',
     method: 'get',
     params
   });
 export const getBaseData = (params: any) =>
   request({
-    url:'system/baseData',
+    url: 'system/baseData',
     method: 'get',
     params
   });
 export const queryTerminal = (params: any) =>
   request({
-    url:'system/car/queryTerminal',
+    url: 'system/car/queryTerminal',
     method: 'get',
     params
   });
 
 export const queryBusinesses = (params: any) =>
   request({
-    url:'system/queryBusinesses',
+    url: 'system/queryBusinesses',
     method: 'get',
     params
   });
 
 
 export const queryDealer = (params: any) =>
-request({
-  url:'system/car/queryDealer',
-  method: 'get',
-  params
-});
+  request({
+    url: 'system/car/queryDealer',
+    method: 'get',
+    params
+  });
 
 
 //防止重复提交
-export const operateCar = (p: object) => post(base + 'system/lockEnable/operat', p, true);
+export const operateCar = (p: object) => post('system/lockEnable/operat', p, true);
 
-export const downloadCar = (p: object) => post(base + 'system/car/downloadCar', p, true);
+export const downloadCar = (p: object) => post('system/car/downloadCar', p, true);
 
 
-export const updateCar = (p: object) => post(base + 'system/car/updateCar', p, true);
+export const updateCar = (p: object) => post('system/car/updateCar', p, true);
